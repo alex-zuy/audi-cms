232e8e9b9d2b97e6a46191e9dc2ce55e567d2ab1
Alexey <zuy_alexey@mail.ru>
2015-08-09 22:12:45 +0300

    Added convenience trait for tests

---
+++ test/utility/FakeAppPerSuite.scala
@@ -0,0 +1,20 @@
+package utility
+
+import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.test.FakeApplication
+import slick.driver.JdbcProfile
+
+/**
+ * Convinience trait for tests that need fake application
+ * and clean or seeded database for tests.
+ */
+trait FakeAppPerSuite extends PlaySpec with OneAppPerSuite with SeededDatabase {
+
+  implicit override lazy val app: FakeApplication = FakeApplication(
+    additionalConfiguration = Map("slick.dbs.default.db.properties.databaseName" -> "audi-cms-test")
+  )
+
+  implicit val dbConfig = DatabaseConfigProvider.get[JdbcProfile]
+
+}
