9ca0d7a5e2fb87d95ab98734b69f3eafb698757f
Alexey <zuy_alexey@mail.ru>
2015-08-18 22:35:12 +0300

    Added convinience helper method to run queries

--- app/internal/DefaultDbConfiguration.scala
+++ app/internal/DefaultDbConfiguration.scala
@@ -3,9 +3,16 @@ package internal
 import play.api.Play.current
 import play.api.db.slick.DatabaseConfigProvider
 import slick.backend.DatabaseConfig
+import slick.dbio
+import slick.dbio.DBIOAction
 import slick.driver.JdbcProfile
 
+import scala.concurrent.Future
+
 trait DefaultDbConfiguration extends DatabaseConfiguration {
+
+  def runQuery[R, S <: dbio.NoStream, E <: dbio.Effect](query: DBIOAction[R,S,E])(implicit dbConfig: DatabaseConfig[JdbcProfile]): Future[R] =
+    dbConfig.db.run(query)
 
   implicit lazy val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfigProvider.get[JdbcProfile]
 }
