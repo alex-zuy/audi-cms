92e9569317ed6ddec00c9ffce49e611b432edfb0
Alexey <zuy_alexey@mail.ru>
2015-08-20 13:03:38 +0300

    Added javascript router for making requests to backend

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import internal.{DefaultDbConfiguration, Authenticate}
 import play.api._
 import play.api.libs.json._
 import play.api.mvc._
+import play.api.routing.JavaScriptReverseRouter
 
 
 class Application extends Controller with DefaultDbConfiguration {
@@ -21,6 +22,38 @@ class Application extends Controller with DefaultDbConfiguration {
   def login = Authenticate.loginAction()
 
   def logout = Authenticate.logoutAction()
+
+  def jsRoutes = Action { implicit request =>
+    Ok(
+      JavaScriptReverseRouter("jsRoutes")(
+        routes.javascript.Application.login,
+        routes.javascript.Application.logout,
+
+        routes.javascript.Managers.list,
+        routes.javascript.Managers.store,
+        routes.javascript.Managers.update,
+        routes.javascript.Managers.delete,
+        routes.javascript.Managers.grantAdmin,
+        routes.javascript.Managers.revokeAdmin,
+        routes.javascript.Managers.changePassword,
+
+        routes.javascript.Contacts.list,
+        routes.javascript.Contacts.store,
+        routes.javascript.Contacts.show,
+        routes.javascript.Contacts.update,
+        routes.javascript.Contacts.delete,
+        routes.javascript.Contacts.storeAddress,
+        routes.javascript.Contacts.updateAddress,
+        routes.javascript.Contacts.deleteAddress,
+        routes.javascript.Contacts.storeNumber,
+        routes.javascript.Contacts.updateNumber,
+        routes.javascript.Contacts.deleteNumber,
+        routes.javascript.Contacts.storeEmail,
+        routes.javascript.Contacts.updateEmail,
+        routes.javascript.Contacts.deleteEmail
+      )
+    ).as("text/javascript")
+  }
 }
 
 object Application {
