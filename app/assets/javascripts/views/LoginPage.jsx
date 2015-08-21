e30728f1239dd8440fbc3d6a77a705b2a022e9c4
Alexey <zuy_alexey@mail.ru>
2015-08-21 17:05:07 +0300

    Implemented login page

--- app/assets/javascripts/views/LoginPage.jsx
+++ app/assets/javascripts/views/LoginPage.jsx
@@ -5,26 +5,36 @@ define(['react', 'react-router', 'mui'], function(React, ReactRouter, mui) {
     var Paper = mui.Paper;
 
     var LoginPage = React.createClass({
+        mixins: [
+            ReactRouter.Navigation,
+        ],
         getInitialState: function() {
             return {
-                buttonDisabled: true
+                buttonDisabled: true,
+                errors: '',
             }
         },
         render: function() {
+            var hasErrors = this.state.errors.length !== 0;
             return (
                 <div className="row" style={{ marginTop: "100px" }}>
                     <div className="col s12 m8 offset-m2 l6 offset-l3">
                         <Paper zDepth={4} rounded={false} style={{padding: "50px"}}>
                             <h5>key.login</h5>
                             <form onChange={this.enableDisableButton}>
+                                <div style={{display: hasErrors ? 'initial' : 'none'}}>
+                                    <div ref="errors" className="card-panel red lighten-1 white-text">{this.state.errors}</div>
+                                </div>
                                 <TextField
                                     ref="email"
+                                    onEnterKeyDown={this.enterPressed}
                                     floatingLabelText="key.email"
                                     fullWidth={true}
                                     hintText="key.enter.login.email"
                                     type="text"/>
                                 <TextField
                                     ref="password"
+                                    onEnterKeyDown={this.enterPressed}
                                     floatingLabelText="key.password"
                                     fullWidth={true}
                                     hintText="key.enter.password"
@@ -43,13 +53,34 @@ define(['react', 'react-router', 'mui'], function(React, ReactRouter, mui) {
                 </div>
             );
         },
+        enterPressed: function() {
+            if(!this.fieldsEmpty()) {
+                this.attemptLogin();
+            }
+        },
+        fieldsEmpty: function() {
+            return this.refs.email.getValue().length === 0 || this.refs.password.getValue().length === 0;
+        },
         attemptLogin: function() {
-            console.log('Attempt login');
+            var loginRoute = jsRoutes.controllers.Application.login();
+            $.ajax({
+                method: loginRoute.type,
+                url: loginRoute.url,
+                contentType: "text/json",
+                data: JSON.stringify({
+                    email: this.refs.email.getValue(),
+                    password: this.refs.password.getValue()
+                }),
+                error: function(xhr, status, error) {
+                    this.setState({errors:'key.no.such.user'});
+                }.bind(this),
+                success: function(data, status) {
+                    this.transitionTo('control-panel')
+                }.bind(this)
+            });
         },
         enableDisableButton: function() {
-            var emailEmpty = this.refs.email.getValue().length === 0;
-            var passwordEmpty = this.refs.password.getValue().length === 0;
-            this.setState({buttonDisabled: emailEmpty || passwordEmpty});
+            this.setState({buttonDisabled: this.fieldsEmpty()});
         }
     });
 
