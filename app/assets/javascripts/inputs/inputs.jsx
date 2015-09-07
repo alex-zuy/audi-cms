f1f982138cab94727305ef15c217aa83a2099209
Alexey <zuy_alexey@mail.ru>
2015-09-07 19:24:23 +0300

    Added convinience agregate module

---
+++ app/assets/javascripts/inputs/inputs.jsx
@@ -0,0 +1,15 @@
+define([
+    'js/inputs/HiddenInput',
+    'js/inputs/MapInput',
+    'js/inputs/TextInput',
+], function(
+    HiddenInput,
+    MapInput,
+    TextInput
+) {
+    return {
+        HiddenInput: HiddenInput,
+        MapInput: MapInput,
+        TextInput: TextInput,
+    };
+});
