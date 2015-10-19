1fc4e9ebeeb894b3ee70add40052ab1647cbc305
Zuy Alexey <zuy_alexey@mail.ru>
2015-10-19 21:00:40 +0300

    Modified main page to display articles

--- app/assets/javascripts/views/MainPage.jsx
+++ app/assets/javascripts/views/MainPage.jsx
@@ -1,13 +1,39 @@
-define(['react', 'javascripts/components/TodoDummy'], function(React, TodoDummy) {
+define(['react', 'allMixins',
+    'javascripts/components/TodoDummy',
+    'js/widgets/Article'
+], function(React, allMixins, TodoDummy, Article) {
 
     var MainPage = React.createClass({
+        mixins: [
+            allMixins.AjaxMixin,
+        ],
+        getInitialState() {
+            return {
+                articles: [],
+            }
+        },
         render: function() {
             return (
                 <div>
-                    <h1>Main page</h1>
-                    <TodoDummy text='Main page'/>
+                    <div className="row">
+                        <div className="col l2 red">
+                        </div>
+                        <div className="col l8 offset-l2">{
+                                this.state.articles.map((a) =>
+                                    <Article article={a}/>
+                                )
+                            }
+                        </div>
+                        <div className="col l2 offset-l10 yellow">
+                        </div>
+                    </div>
                 </div>
             );
+        },
+        componentDidMount() {
+            this.ajax(jsRoutes.controllers.Articles.list(), {
+                success: (articles) => this.setState({articles: articles})
+            })
         }
     });
 
