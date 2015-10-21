5338bf65e43ec26ada96d7fef20b076b9309bbe9
Zuy Alexey <zuy_alexey@mail.ru>
2015-10-21 23:23:18 +0300

    Added basic display of articles at news & offers page

--- app/assets/javascripts/views/NewsAndOffersPage.jsx
+++ app/assets/javascripts/views/NewsAndOffersPage.jsx
@@ -1,13 +1,41 @@
-define(['react', 'javascripts/components/TodoDummy',], function(React, TodoDummy) {
+define(['react', 'allMixins',
+    'js/widgets/ModelThumbnail',
+    'js/widgets/Article',
+], function(React, allMixins, ModelThumbnail, Article) {
 
     var NewsAndOffersPage = React.createClass({
+        mixins: [
+            allMixins.AjaxMixin,
+        ],
+        getInitialState() {
+            return {articles: []}
+        },
         render: function() {
             return (
                 <div>
-                    <h1>News and offers page</h1>
-                    <TodoDummy text='News and offers page'/>
+                    <div className="row">
+                        <div className="col l3 m3 hide-on-small-only">
+                            <div style={{marginTop: '20px'}}>
+                                <ModelThumbnail/>
+                            </div>
+                        </div>
+                        <div className="col l9 m9">{
+                            this.state.articles.map((a) =>
+                                <Article article={a}/>
+                            )
+                        }
+                        </div>
+                    </div>
                 </div>
             )
+        },
+        componentDidMount() {
+            this.ajax(jsRoutes.controllers.Articles.list(), {
+                success: (allArticles) => {
+                    const articles = allArticles.filter((article) => _.contains(['news', 'offers'], article.category));
+                    this.setState({articles: articles});
+                }
+            });
         }
     });
 
