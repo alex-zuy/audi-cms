02b7c4fa10adcb85b709f9ea83810de7007d56a6
Zuy Alexey <zuy_alexey@mail.ru>
2015-11-04 21:24:55 +0300

    Basic implementation of ServiceTerms page

--- app/assets/javascripts/views/ServiceTerms.jsx
+++ app/assets/javascripts/views/ServiceTerms.jsx
@@ -1,11 +1,59 @@
-define(['react'], function (React) {
+define(['react', 'allMixins', 'mui'], function (React, allMixins, mui) {
+
+    const {CircularProgress, Card, CardMedia, CardTitle} = mui;
+
     return React.createClass({
+        mixins: [
+            allMixins.IntlMixin,
+            allMixins.AjaxMixin,
+        ],
+        getDefaultProps() {
+            return {
+                msgKeyPrefix: 'servicePage',
+            };
+        },
+        getInitialState() {
+            return {
+                articles: null,
+            };
+        },
         render() {
             return (
                 <div>
-                    <h1>Service Terms Page</h1>
+                    <h1>{this.getMsg('labels.termsOfService')}</h1>
+                    <div>{(() => {
+                        if(_.isNull(this.state.articles)) {
+                            return <CircularProgress/>;
+                        }
+                        else {
+                            return (
+                                <div className="row">{_.shuffle(this.getServiceArticles()).map(article =>
+                                    <div className="col l4 m6">
+                                        <a href="">
+                                            <Card>
+                                                <CardMedia
+                                                    overlay={<CardTitle title={this.getPreferedText(article.title)}/>}>
+                                                    <img src={jsRoutes.controllers.Photos.showImage(_.first(_.shuffle(article.photos)).id).url}/>
+                                                </CardMedia>
+                                            </Card>
+                                        </a>
+                                    </div>
+                                )}
+                                </div>
+                            );
+                        }
+                    })()}
+                    </div>
                 </div>
             );
+        },
+        componentDidMount() {
+            this.ajax(jsRoutes.controllers.Articles.list(), {
+                success: (all) => this.setState({articles: all})
+            });
+        },
+        getServiceArticles() {
+            return _.where(this.state.articles, {category: 'service'});
         }
     });
 });
